c61fef4f1047d379e855947cdf6861f34540eaf9
fridabu <f0504070002@gmail.com>
2020-04-22 15:04:05 +0300

    adding get cars call

--- public/docs/js/album.js
+++ public/docs/js/album.js
@@ -6,7 +6,28 @@ const reserveButton = document.getElementById("reserve_button");
 
 
 
-searchButton.addEventListener("click", () =>{
+searchButton.addEventListener("click", () => {
     console.log(fromDateElement.value);
+    giveResults();
 
 });
+
+const giveResults = function () {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                var data = JSON.parse(xhr.responseText);
+                const cars_row = document.getElementById("cars_row");
+                for (let i = 0; i < cars_row.children.length; i++) {
+                   cars_row.children[i].getElementsByTagName('text')[0].innerHTML = data[i].make;
+                }
+            }
+            else {
+                console.error(xhr.responseText);
+            }
+        }
+    }
+    xhr.open('GET', '/getCar', true);
+    xhr.send();
+}
