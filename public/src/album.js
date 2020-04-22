fd2b2bf879afe69a756b0b7fa4385f62ac08b7e4
fridabu <f0504070002@gmail.com>
2020-04-22 11:49:31 +0300

    UI adding basic js file for dom manuplation on album.js

---
+++ public/src/album.js
@@ -0,0 +1,12 @@
+
+const fromDateElement = document.getElementById("begin_date");
+const toDateElement = document.getElementById("end_date");
+const searchButton = document.getElementById("search_button");
+const reserveButton = document.getElementById("reserve_button");
+
+
+
+searchButton.addEventListener("click", () =>{
+    console.log(fromDateElement.value);
+
+});
